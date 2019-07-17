9c7a02e1a6fa35f8b222b778ec0ca370f4a12dd1
Johannes Wolters <wolters.joh@web.de>
2019-07-17 22:45:44 +0200

    included footer w/php, fixed mobile

---
+++ datenschutz.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- FAVICON-->
+	<link rel="icon" href="img/favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+
+	<!-- TITLE -->
+	<title>Lernen Sie mich näher kennen</title>
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/custom.css">
+
+	<!-- JS -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+
+	<section id="header-impressum">
+
+		<div class="bg-white-impressum"></div>
+		<div class="col-12 text-center">
+			<h1>Datenschutz</h1>
+		</div>
+
+
+	</section>
+
+	<!-- SCROLL TO TOP BUTTON -->
+	<div class="scrolltotop"><div class="scrolltotop-finger"></div></div>
+
+
+	<div class="container section-impressum">
+		<div class="row">
+			<div class="col-12">
+				<p>Verantwortlicher im Sinne der Datenschutzgesetze, insbesondere der EU-Datenschutzgrundverordnung (DSGVO), ist:<br><br>
+					Johannes Wolters<br>
+					Hartweg 30<br>
+					52525 Waldfeucht<br>
+					Deutschland</p>
+					<hr><p></p>
+					<h2>Ihre Betroffenenrechte</h2>
+					<p>Unter den angegebenen Kontaktdaten können Sie jederzeit folgende Rechte ausüben:</p>
+					<ul>
+						<li>Auskunft über Ihre bei uns gespeicherten Daten und deren Verarbeitung (Art. 15 DSGVO),</li>
+						<li>Berichtigung unrichtiger personenbezogener Daten (Art. 16 DSGVO),</li>
+						<li>Löschung Ihrer bei uns gespeicherten Daten (Art. 17 DSGVO),</li>
+						<li>Einschränkung der Datenverarbeitung, sofern wir Ihre Daten aufgrund gesetzlicher Pflichten noch nicht löschen dürfen (Art. 18 DSGVO),</li>
+						<li>Widerspruch gegen die Verarbeitung Ihrer Daten bei uns (Art. 21 DSGVO) und</li>
+						<li>Datenübertragbarkeit, sofern Sie in die Datenverarbeitung eingewilligt haben oder einen Vertrag mit uns abgeschlossen haben (Art. 20 DSGVO).</li>
+					</ul>
+					<br><br>
+					<p>Sofern Sie mir eine Einwilligung erteilt haben, können Sie diese jederzeit mit Wirkung für die Zukunft widerrufen.<br><br>
+						Sie können sich jederzeit mit einer Beschwerde an eine Aufsichtsbehörde wenden, z. B. an die zuständige Aufsichtsbehörde des Bundeslands Ihres Wohnsitzes oder an die für mir als verantwortliche Stelle zuständige Behörde.<br><br>
+						Eine Liste der Aufsichtsbehörden (für den nichtöffentlichen Bereich) mit Anschrift finden Sie unter: <a href="https://www.bfdi.bund.de/DE/Infothek/Anschriften_Links/anschriften_links-node.html" target="_blank" rel="nofollow noopener">https://www.bfdi.bund.de/DE/Infothek/Anschriften_Links/anschriften_links-node.html</a>.</p>
+						<hr><p></p>
+						<h2>Kontaktformular</h2>
+						<h3>Art und Zweck der Verarbeitung:</h3>
+						<p>Die von Ihnen eingegebenen Daten werden zum Zweck der individuellen Kommunikation mit Ihnen gespeichert. Hierfür ist die Angabe einer validen E-Mail-Adresse sowie Ihres Namens erforderlich. Diese dient der Zuordnung der Anfrage und der anschließenden Beantwortung derselben. Die Angabe weiterer Daten ist optional.</p>
+						<h3>Rechtsgrundlage:</h3>
+						<p>Die Verarbeitung der in das Kontaktformular eingegebenen Daten erfolgt auf der Grundlage eines berechtigten Interesses (Art. 6 Abs. 1 lit. f DSGVO).<br><br>
+							Durch Bereitstellung des Kontaktformulars möchten wir Ihnen eine unkomplizierte Kontaktaufnahme ermöglichen. Ihre gemachten Angaben werden zum Zwecke der Bearbeitung der Anfrage sowie für mögliche Anschlussfragen gespeichert.<br><br>
+							Sofern Sie mit uns Kontakt aufnehmen, um ein Angebot zu erfragen, erfolgt die Verarbeitung der in das Kontaktformular eingegebenen Daten zur Durchführung vorvertraglicher Maßnahmen (Art. 6 Abs. 1 lit. b DSGVO).</p>
+							<h3>Empfänger:</h3>
+							<p>Empfänger der Daten sind ggf. Auftragsverarbeiter.</p>
+							<h3>Speicherdauer:</h3>
+							<p>Daten werden spätestens 6 Monate nach Bearbeitung der Anfrage gelöscht.<br><br>
+								Sofern es zu einem Vertragsverhältnis kommt, unterliege ich den gesetzlichen Aufbewahrungsfristen nach HGB und löschen Ihre Daten nach Ablauf dieser Fristen.</p>
+								<h3>Bereitstellung vorgeschrieben oder erforderlich:</h3>
+								<p>Die Bereitstellung Ihrer personenbezogenen Daten erfolgt freiwillig. Ich kann Ihre Anfrage jedoch nur bearbeiten, sofern Sie mir Ihren Namen, Ihre E-Mail-Adresse und den Grund der Anfrage mitteilen.</p>
+								<hr><p></p>
+								<h2>Verwendung von Scriptbibliotheken (Google Webfonts)</h2>
+								<h3>Art und Zweck der Verarbeitung:</h3>
+								<p>Um meine Inhalte browserübergreifend korrekt und grafisch ansprechend darzustellen, verwende ich auf dieser Website „Google Web Fonts“ der Google LLC (1600 Amphitheatre Parkway, Mountain View, CA 94043, USA; nachfolgend „Google“) zur Darstellung von Schriften.<br><br>
+									Die Datenschutzrichtlinie des Bibliothekbetreibers Google finden Sie hier: <a href="https://www.google.com/policies/privacy/">https://www.google.com/policies/privacy/</a></p>
+									<h3>Rechtsgrundlage:</h3>
+									<p>Rechtsgrundlage für die Einbindung von Google Webfonts und dem damit verbundenen Datentransfer zu Google ist Ihre Einwilligung (Art. 6 Abs. 1 lit. a DSGVO). </p>
+									<h3>Empfänger:</h3>
+									<p>Der Aufruf von Scriptbibliotheken oder Schriftbibliotheken löst automatisch eine Verbindung zum Betreiber der Bibliothek aus. Dabei ist es theoretisch möglich – aktuell allerdings auch unklar ob und ggf. zu welchen Zwecken – dass der Betreiber in diesem Fall Google Daten erhebt.</p>
+									<h3>Speicherdauer:</h3>
+									<p>Ich erhebe keine personenbezogenen Daten, durch die Einbindung von Google Webfonts.<br><br>
+										Weitere Informationen zu Google Web Fonts finden Sie unter <a href="https://developers.google.com/fonts/faq">https://developers.google.com/fonts/faq</a> und in der Datenschutzerklärung von Google: <a href="https://www.google.com/policies/privacy/">https://www.google.com/policies/privacy/</a>.</p>
+										<h3>Drittlandtransfer:</h3>
+										<p>Google verarbeitet Ihre Daten in den USA und hat sich dem EU_US Privacy Shield unterworfen <a href="https://www.privacyshield.gov/EU-US-Framework">https://www.privacyshield.gov/EU-US-Framework</a>.</p>
+										<h3>Bereitstellung vorgeschrieben oder erforderlich:</h3>
+										<p>Die Bereitstellung der personenbezogenen Daten ist weder gesetzlich, noch vertraglich vorgeschrieben. Allerdings kann ggfs. die korrekte Darstellung der Inhalte durch Standardschriften nicht möglich sein.</p>
+										<h3>Widerruf der Einwilligung:</h3>
+										<p>Zur Darstellung der Inhalte wird regelmäßig die Programmiersprache JavaScript verwendet. Sie können der Datenverarbeitung daher widersprechen, indem Sie die Ausführung von JavaScript in Ihrem Browser deaktivieren oder einen JavaScript-Blocker installieren. Bitte beachten Sie, dass es hierdurch zu Funktionseinschränkungen auf der Website kommen kann.</p>
+										<hr><p></p>
+										<h2>SSL-Verschlüsselung</h2>
+										<p>Um die Sicherheit Ihrer Daten bei der Übertragung zu schützen, verwende ich dem aktuellen Stand der Technik entsprechende Verschlüsselungsverfahren (z. B. SSL) über HTTPS.</p>
+										<hr><p></p>
+										<h2>Änderung der Datenschutzbestimmungen</h2>
+										<p>Ich behalte mir vor, diese Datenschutzerklärung anzupassen, damit sie stets den aktuellen rechtlichen Anforderungen entspricht oder um Änderungen meiner Leistungen in der Datenschutzerklärung umzusetzen, z.B. bei der Einführung neuer Services. Für Ihren erneuten Besuch gilt dann die neue Datenschutzerklärung.</p>
+										<hr><p></p>
+										<h2>Fragen an den Datenschutzbeauftragten</h2>
+										<p>Wenn Sie Fragen zum Datenschutz haben, schreiben Sie mir bitte eine E-Mail.</p>
+
+										<p></p><p><em>Die Datenschutzerklärung wurde mithilfe der activeMind AG erstellt, den Experten für <a href="https://www.activemind.de/datenschutz/datenschutzhinweis-generator/" target="_blank" rel="noopener">externe Datenschutzbeauftragte</a> (Version #2019-04-10).</em></p>
+									</div>
+								</div>
+							</div>
+
+
+
+							<!-- FOOTER -->
+							<div class="triangle-section">
+								<div class="triangle-footer"></div>
+							</div>
+							<?php include("footer.html");?>
+
+
+							<script src="js/custom.js"></script>
+							</body>
+							</html>
